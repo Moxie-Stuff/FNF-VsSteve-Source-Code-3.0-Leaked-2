2056bcb5a786fb695fbe7a0baf209e39fdf62e14
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2023-03-31 15:55:20 +0300

    Update WarnCreditState.hx

--- source/WarnCreditState.hx
+++ source/WarnCreditState.hx
@@ -38,15 +38,33 @@ class WarnCreditState extends MusicBeatState
 		kadeLogo.y -= 180;
 		kadeLogo.alpha = 0.8;
 		add(kadeLogo);
-		
+
+		#if mobileC
 		var txt:FlxText = new FlxText(0, 0, FlxG.width,
+			"You are entering an Unfinished Corner of the Mod\n"
+			+ "Currently, this menu is incomplete and Broken, so please be patient. Everything will be complete in the Full Release Update.\n"
+			+ "If you want to still check the current Menu then Press A, Press B to go Back. \n"
+			+ "Be aware that the text might be broken.\n"
+			+ "Press A on an Icon to Visit their Social Media (If Available)"
+			);
+		#elseif mobileCweb
+                var txt:FlxText = new FlxText(0, 0, FlxG.width,
+			"You are entering an Unfinished Corner of the Mod\n"
+			+ "Currently, this menu is incomplete and Broken, so please be patient. Everything will be complete in the Full Release Update.\n"
+			+ "If you want to still check the current Menu then Press Enter/A, Press Escape/Backspace/B to go Back. \n"
+			+ "Be aware that the text might be broken.\n"
+			+ "Press Enter/A on an Icon to Visit their Social Media (If Available)"
+			);
+                #elseif (!mobileC || !mobileCweb)
+                var txt:FlxText = new FlxText(0, 0, FlxG.width,
 			"You are entering an Unfinished Corner of the Mod\n"
 			+ "Currently, this menu is incomplete and Broken, so please be patient. Everything will be complete in the Full Release Update.\n"
 			+ "If you want to still check the current Menu then Press Enter, Press Escape/Backspace to go Back. \n"
 			+ "Be aware that the text might be broken.\n"
 			+ "Press Enter on an Icon to Visit their Social Media (If Available)"
 			);
-		
+                #end
+
 		txt.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.fromRGB(200, 200, 200), CENTER);
 		txt.borderColor = FlxColor.BLACK;
 		txt.borderSize = 3;
@@ -75,6 +93,8 @@ class WarnCreditState extends MusicBeatState
 			if(kadeLogo.alpha == 0.8) FlxTween.tween(kadeLogo, {alpha: 1}, 0.8, {ease: FlxEase.quartInOut});
 			else FlxTween.tween(kadeLogo, {alpha: 0.8}, 0.8, {ease: FlxEase.quartInOut});
 		}, 0);
+
+                #if (mobileC || mobileCweb) addVirtualPad(NONE, A_B); #end
 	}
 
 	override function update(elapsed:Float)
