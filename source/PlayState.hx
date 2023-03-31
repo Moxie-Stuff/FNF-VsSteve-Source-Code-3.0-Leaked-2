b61fd17c71dd45026064fcd5c84f6823365bb925
mcagabe19 <egzozu.be.bas@gmail.com>
2023-03-31 21:07:35 +0300

    ...

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2934,7 +2934,7 @@ class PlayState extends MusicBeatState
 			babyArrow.x += 50;
 			babyArrow.x += ((FlxG.width / 2) * player);
 			
-			if (FlxG.save.data.middleScroll && SONG.song.toLowerCase() != 'bos' #if web || SONG.song.toLowerCase() != 'revenge' #end || SONG.song.toLowerCase() != 'espionage' || SONG.song.toLowerCase() != 'bonk' || SONG.song.toLowerCase() != 'entity') {
+			if (FlxG.save.data.middleScroll && SONG.song.toLowerCase() != 'bos' #if !web || SONG.song.toLowerCase() != 'revenge' #end || SONG.song.toLowerCase() != 'espionage' || SONG.song.toLowerCase() != 'bonk' || SONG.song.toLowerCase() != 'entity') {
 				babyArrow.x -= 275;
 				if (player != 1) {
 					babyArrow.visible = false;
